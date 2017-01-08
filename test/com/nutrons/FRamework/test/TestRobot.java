9a93821a1bad99ece1436155a3d074135443a9c4
Milo Davis <davis.mil@husky.neu.edu>
2017-01-08 14:29:06 -0500

    Added empty test class

---
+++ test/com/nutrons/FRamework/test/TestRobot.java
@@ -0,0 +1,14 @@
+package com.nutrons.FRamework.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestRobot {
+
+	@Test
+	public void test() {
+		assertEquals(1, 1);
+	}
+
+}
