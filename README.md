f05da22aae03d423dd9346a704cc0f61a8398b7a
Milo Davis <davis.mil@husky.neu.edu>
2017-01-12 10:32:42 -0500

    Initial commit and CI setup

---
+++ README.md
@@ -0,0 +1,4 @@
+[![Build Status](https://travis-ci.org/FRC125/FRamework.svg?branch=master)](https://travis-ci.org/FRC125/FRamework)
+
+# FRamework
+A functional-reactive wrapper around WPILib. 
---
+++ src/com/nutrons/FRamework/Robot.java
@@ -0,0 +1,13 @@
+package com.nutrons.FRamework;
+
+import edu.wpi.first.wpilibj.RobotBase;
+
+public class Robot extends RobotBase {
+
+    @Override
+    public void startCompetition() {
+	// TODO Auto-generated method stub
+	
+    }
+    
+}
