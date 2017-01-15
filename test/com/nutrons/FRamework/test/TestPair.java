99c8e69dabe8b7cb278d3f1ca3ca587d9a3301b8
Asher Gottlieb <totally.ashes@gmail.com>
2017-01-15 12:11:10 -0500

    added Pair utility class

---
+++ test/com/nutrons/FRamework/test/TestPair.java
@@ -0,0 +1,31 @@
+package com.nutrons.FRamework.test;
+
+import com.nutrons.FRamework.Pair;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class TestPair {
+
+    public Pair<Integer, String> p;
+    @Before
+    public void initialize() {
+        p = new Pair<>(1, "b");
+    }
+
+    @Test
+    public void checkString() {
+        assertEquals(p.toString(), "( 1, b )");
+    }
+
+    @Test
+    public void checkEquality() {
+        assertEquals(p, new Pair<Integer, String>(1, "b"));
+    }
+
+    @Test
+    public void checkInequality() {
+        assertEquals(p, new Pair<Integer, String>(2, "b"));
+    }
+}
