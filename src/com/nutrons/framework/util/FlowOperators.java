34cf8d3ffbc02a6c7c4a06c0ad3262d724dd813b
cappuccinocat <xinglydia@gmail.com>
2017-02-01 19:14:48 -0500

    made pull request changes

--- src/com/nutrons/framework/util/FlowOperators.java
+++ src/com/nutrons/framework/util/FlowOperators.java
@@ -36,4 +36,6 @@ public class FlowOperators {
   public static Flowable<Double> deadband(Flowable<Double> input) {
     return input.map((x) -> abs(x) < 0.2 ? 0.0 : x);
   }
+
+  public static <T> T getLastValue(Flowable<T>  input) { return input.blockingLatest().iterator().next(); }
 }
