cc56e00b7eab79ad974dc8c63179fe8b0d8b57fa
AndreasLc1103 <darkninjawolf1@gmail.com>
2017-02-11 17:05:51 -0500

    Added common contoller class with lgotiech method

---
+++ src/com/nutrons/framework/inputs/CommonController.java
@@ -0,0 +1,93 @@
+package com.nutrons.framework.inputs;
+
+import io.reactivex.Flowable;
+
+/**
+ * A WpiGamepad with default configuration for an Xbox360 controller.
+ */
+public class CommonController extends WpiGamepad {
+  private int xButton;
+  private int aButton;
+  private int bButton;
+  private int yButton;
+  private int startButton;
+  private int rightBumper;
+  private int leftBumper;
+  private int selectButton;
+
+  private CommonController(int port,
+                           int axis1X,
+                           int axis1Y,
+                           int axis2X,
+                           int axis2Y,
+                           int buttonX,
+                           int buttonY,
+                           int buttonB,
+                           int buttonA,
+                           int buttonStart,
+                           int ButtonSelect,
+                           int bumperRight,
+                           int bumperleft) {
+    super(port, axis1X, axis1Y, axis2X, axis2Y);
+    this.xButton = buttonX;
+    this.yButton = buttonY;
+    this.bButton = buttonB;
+    this.aButton = buttonA;
+    this.startButton = buttonStart;
+    this.selectButton = ButtonSelect;
+    this.rightBumper = bumperRight;
+    this.leftBumper = bumperleft;
+
+
+  }
+
+  public static CommonController logitech(int port) {
+    return new CommonController(
+        port,
+        InputMap.LOGITECH_X_1,
+        InputMap.LOGITECH_Y_1,
+        InputMap.LOGITECH_X_2,
+        InputMap.LOGITECH_Y_2,
+        InputMap.LOGITECH_X,
+        InputMap.LOGITECH_Y,
+        InputMap.LOGITECH_B,
+        InputMap.LOGITECH_A,
+        InputMap.LOGITECH_START,
+        InputMap.LOGITECH__SELECT,
+        InputMap.LOGITECH_RIGHT_BUMPER,
+        InputMap.LOGITECH_LEFT_BUMPER);
+  }
+
+
+  public Flowable<Boolean> xButton() {
+    return button(xButton);
+  }
+
+  public Flowable<Boolean> aButton() {
+    return button(aButton);
+  }
+
+  public Flowable<Boolean> yButton() {
+    return button(yButton);
+  }
+
+  public Flowable<Boolean> bButton() {
+    return button(bButton);
+  }
+
+  public Flowable<Boolean> startButton() {
+    return button(startButton);
+  }
+
+  public Flowable<Boolean> selectButton() {
+    return button(selectButton);
+  }
+
+  public Flowable<Boolean> rightBumper() {
+    return button(rightBumper);
+  }
+  public Flowable<Boolean> leftBumper(){
+    return button(leftBumper);
+  }
+}
+
--- src/com/nutrons/framework/inputs/InputMap.java
+++ src/com/nutrons/framework/inputs/InputMap.java
@@ -1,9 +1,22 @@
 package com.nutrons.framework.inputs;
 
 class InputMap {
-
+  // Xbox360 Controller Map
   static final int XBOX_X_1 = 0;
   static final int XBOX_X_2 = 4;
   static final int XBOX_Y_1 = 1;
   static final int XBOX_Y_2 = 5;
+  // Logitech Controller Map
+  static final int LOGITECH_Y_1 = 1;
+  static final int LOGITECH_Y_2 = 5;
+  static final int LOGITECH_X_1 = 0;
+  static final int LOGITECH_X_2 = 4;
+  static final int LOGITECH_X = 3;
+  static final int LOGITECH_B = 2;
+  static final int LOGITECH_A = 1;
+  static final int LOGITECH_Y = 4;
+  static final int LOGITECH_START = 8;
+  static final int LOGITECH__SELECT = 7;
+  static final int LOGITECH_RIGHT_BUMPER = 6;
+  static final int LOGITECH_LEFT_BUMPER = 5;
 }
