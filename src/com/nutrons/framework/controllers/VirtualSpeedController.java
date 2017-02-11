ac19bcd793369a843cc124b9a86bd1341a5ddf55
cappuccinocat <xinglydia@gmail.com>
2017-02-10 20:33:42 -0500

    got rid of override methods, again

--- src/com/nutrons/framework/controllers/VirtualSpeedController.java
+++ src/com/nutrons/framework/controllers/VirtualSpeedController.java
@@ -9,21 +9,6 @@ public class VirtualSpeedController extends LoopSpeedController {
   }
 
   @Override
-  public void resetPositionTo(double position) {
-
-  }
-
-  @Override
-  public double position() {
-    return 0;
-  }
-
-  @Override
-  public void setOutputVoltage(double min, double max) {
-
-  }
-
-  @Override
   public void accept(ControllerEvent controllerEvent) {
     controllerEvent.actOn(this);
   }
