fab0a9039e0ea455e3ed666260eeaf946a6a5076
AndreasLc1103 <darkninjawolf1@gmail.com>
2017-02-13 15:53:54 -0500

    fixed casing

--- src/com/nutrons/framework/controllers/Events.java
+++ src/com/nutrons/framework/controllers/Events.java
@@ -36,12 +36,12 @@ public class Events {
   public static ControllerEvent pid(double pval, double ival, double dval, double fval) {
     return new LoopPropertiesEvent(pval, ival, dval, fval);
   }
-  public static ControllerEvent combine(ControllerEvent... Events) {
+  public static ControllerEvent combine(ControllerEvent... events) {
     return new ControllerEvent() {
       @Override
       public void actOn(Talon talon) {
-        for (ControllerEvent E : Events) {
-          E.actOn(talon);
+        for (ControllerEvent e : events) {
+          e.actOn(talon);
         }
       }
     };
