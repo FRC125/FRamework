4c0d7674e46584ad1a90880e5b2db214c32b1bd8
AndreasLc1103 <darkninjawolf1@gmail.com>
2017-02-23 18:49:46 -0500

    (Brian) added super constructor to tuneables that will let default values go to 0 if wanted, changed getPID method to ControllerEvent

--- src/com/nutrons/framework/controllers/TuneablePID.java
+++ src/com/nutrons/framework/controllers/TuneablePID.java
@@ -8,16 +8,20 @@ public class TuneablePID {
   private final Tuneable tuneableD;
   private final Tuneable tuneableF;
   
-  public TuneablePID(String label) {
+  public TuneablePID(String label, double defaultP, double defaultI, double defaultD, double defaultF) {
     this.label = label;
-    this.tuneableP = new Tuneable(label + "P", 0);
-    this.tuneableI = new Tuneable(label + "I", 0);
-    this.tuneableD = new Tuneable(label + "D", 0);
-    this.tuneableF = new Tuneable(label + "F", 0);
+    this.tuneableP = new Tuneable(label + "_P", defaultP);
+    this.tuneableI = new Tuneable(label + "_I", defaultI);
+    this.tuneableD = new Tuneable(label + "_D", defaultD);
+    this.tuneableF = new Tuneable(label + "_F", defaultF);
   }
 
-  public LoopPropertiesEvent getPID() {
-    return new LoopPropertiesEvent(tuneableP.get(), tuneableI.get(), tuneableD.get(),
+  public TuneablePID(String label) {
+    this(label, 0, 0, 0, 0);
+  }
+
+  public ControllerEvent getPID() {
+    return Events.pid(tuneableP.get(), tuneableI.get(), tuneableD.get(),
         tuneableF.get());
   }
 }
