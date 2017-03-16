225330a8353543f2b80f7e86e0ebf4ea6fb86395
Brian Estevez <goodguybry.be@gmail.com>
2017-03-16 19:23:06 -0400

    Fixed test checks

--- test/com/nutrons/framework/test/TestPID.java
+++ test/com/nutrons/framework/test/TestPID.java
@@ -1,7 +1,7 @@
 package com.nutrons.framework.test;
 
-import static com.nutrons.framework.util.FlowOperators.pidLoop;
 import static com.nutrons.framework.util.FlowOperators.toFlow;
+import static com.nutrons.framework.util.FlowOperators.veryBadDontUseEverPidLoop;
 
 import io.reactivex.Flowable;
 import org.junit.Test;
@@ -11,7 +11,7 @@ public class TestPID {
   @Test
   public void testPIDConcurrency() {
     Flowable<Double> input = toFlow(() -> 1.0);
-    Flowable<Double> output = input.compose(pidLoop(1, 1, 0, 0));
+    Flowable<Double> output = input.compose(veryBadDontUseEverPidLoop(1, 1, 0, 0));
     output.subscribe(x -> {
     }); // tests that this is done on an io scheduler
   }
