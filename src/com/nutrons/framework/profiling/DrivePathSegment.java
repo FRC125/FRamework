6327202c5f338b8c742d1ff357ebc71e2aeebfdb
Josh Young <SourceEpicSquidx@gmail.com>
2017-03-23 20:01:07 -0400

    Updated

--- src/com/nutrons/framework/profiling/DrivePathSegment.java
+++ src/com/nutrons/framework/profiling/DrivePathSegment.java
@@ -2,9 +2,6 @@ package com.nutrons.framework.profiling;
 
 import org.apache.tools.ant.types.Path;
 
-/**
- * Created by Josh on 3/21/2017.
- */
 public class DrivePathSegment {
   private PathSegment left;
   private PathSegment right;
